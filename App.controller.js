30a434d214b5a66d3a323a73bf23df15826265ec
arunsanthanam <arunsanthanam@hotmail.com>
2017-07-18 09:49:47 +0530

    Made small changes to JSON to jSON

--- App.controller.js
+++ App.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
                "sap/ui/core/mvc/Controller",
                "sap/ui/model/json/JSONModel"
-               ], function (Controller,JSONModel) {
+               ], function (Controller,jSONModel) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.roll.controller.App", {
 
